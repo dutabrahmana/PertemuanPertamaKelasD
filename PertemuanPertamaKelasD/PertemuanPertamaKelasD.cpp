41d1e59e9958877398bc18a6d1889e9e37885f42
dutabrahmana <dutalinggau25@gmail.com>
2024-02-27 11:16:14 +0700

    Add project files.

---
+++ PertemuanPertamaKelasD/PertemuanPertamaKelasD.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{//begin
+
+	//numeric nAlas, nTinggi, nLuas
+	//display 'Masukan Alasnya= '
+	//accept nAlas
+	//display 'Masukan Tingginya= '
+	//accept nTinggi
+	//compute nLuas = 1/2 * nAlas * nTinggi
+	//display 'Luasnya adalah = ' + nLuas
+
+	int nAlas, nTinggi;
+	float nLuas;
+	cout << "Masukan Alasnya= ";
+	cin >> nAlas;
+	cout << "Masukan Tingginya= ";
+	cin >> nTinggi;
+	nLuas = 0.5 * (nAlas * nTinggi);
+	cout << "Luasnya Adalah= " << nLuas << endl;
+
+	system("pause");
+}//end
+
+
+
+
+
+
+
+
+
